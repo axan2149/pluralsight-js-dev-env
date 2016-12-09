c4555903af32d88b8d88095164df9aa36032a97c
Martin Rozycki <mrozycki@imshome.com>
2016-12-09 14:37:15 -0500

    added travis integration server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('S0,0.00');
+// debugger; //eslint-disable-line
+console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+
+
