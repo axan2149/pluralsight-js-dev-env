4023e0cf9e0e61d2445473854ae182152c184497
Martin Rozycki <mrozycki@imshome.com>
2016-12-09 14:31:32 -0500

    after Automation module

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,8 +1,7 @@
 /**
  * Created by axan on 12/8/2016.
  */
-var chalk = require("chalk");
-
-
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
+
